bf279185b06a4415d2de08e62d9b8e84f53e8ab4
David Montesinos <davidmr_2012@hotmail.com>
2021-05-31 21:20:18 +0200

    aa

--- rutas/convocatorias.js
+++ rutas/convocatorias.js
@@ -126,7 +126,6 @@ router.get('/equipo/:idEquipo/partidos', validates.protegerRuta(''), (req, res) 
 router.get('/equipo/:idEquipo/entrenamientos', validates.protegerRuta(''), (req, res) => {
     let resultado = [];
     const BaseUrl = 'http://' + req.headers.host + '/public/uploads/';
-
     console.log(BaseUrl);
     Convocatoria.find({ equipo: req.params['idEquipo'], tipo: 'entrenamiento' }).then(x => {
         if (x.length > 0) {
@@ -136,7 +135,7 @@ router.get('/equipo/:idEquipo/entrenamientos', validates.protegerRuta(''), (req,
             });
             res.send({ ok: true, resultado: x });
         } else {
-            res.status(500).send({ ok: false, error: "No se encontraron entrenamientos" })
+            res.status(500).send({ ok: false, error: "No se encontraron partidos" })
         }
     }).catch(err => {
         res.status(500).send({
